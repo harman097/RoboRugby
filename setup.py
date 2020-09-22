1ab0aeb6bb03b80568aa205b361e770577a4748a
Skyler Kehren <pyrodogg@gmail.com>
2020-09-22 19:53:43 +0300

    Add torch and matplotlib to setup dependencies

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='robo_rugby',
       version='0.0.1',
-      install_requires=['gym', 'pygame', 'imageio', 'imageio_ffmpeg']  # And any other dependencies foo needs
+      install_requires=['gym', 'pygame', 'imageio', 'imageio_ffmpeg','torch','matplotlib']  # And any other dependencies robo_rugby needs
 )
